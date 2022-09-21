eb1857f4b1ed1905a947c71fc99ccc80d51746fa
Farrukh Malek <jfukki@gmail.com>
2022-09-22 03:45:53 +0500

    user amine list

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,6 +31,6 @@ class LoginController extends Controller
                 return back()->with('status', "Invalid Login Credentials");
         }
 
-        return redirect()->route('dashboard');
+        return redirect()->route('my-list');
     }
 }
