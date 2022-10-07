2c008ac92209dafc9c1522d5079c2946e2d17032
Farrukh Malek <jfukki@gmail.com>
2022-10-08 02:29:46 +0500

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
 
 
 
-      return  $anime_reviews = DB::table('anime_reviews')
+        $anime_reviews = DB::table('anime_reviews')
         ->select('anime_reviews.review_title', 'anime_reviews.id', 'animes.english_title', 'animes.japanese_title',
          'animes.anime_image', 'anime_reviews.review_text', 'user_details.user_avatar', 'anime_reviews.user_name' )
         ->join('animes', 'anime_reviews.anime_id', '=', 'animes.anime_id')   
