5a2be9d9d125cca1d5b14424cad58a7124944b95
Farrukh Malik <jfukki@gmail.com>
2022-10-07 19:03:48 +0500

    changes

--- resources/views/components/reviewList.blade.php
+++ resources/views/components/reviewList.blade.php
@@ -5,60 +5,60 @@
 
      
 
-    @if(count($anime_reviews) > 0)
+                    @if(count($anime_reviews) > 0)
 
 
-        @foreach($anime_reviews as $anime_review)
+                        @foreach($anime_reviews as $anime_review)
 
-        <div class="col-lg-4 col-12 mb-5 ">
-        
-            <div class=" review-list-home-row"    >
+                        <div class="col-lg-4 col-12 mb-5 ">
+                        
+                            <div class=" review-list-home-row"    >
 
-            <img class="image" src="{{$anime_review->anime_image}}" alt="" />
+                                             <img class="image" src="{{$anime_review->anime_image}}" alt="" />
 
-                            <h2 class="review-card-title-test">
-                              {{Str::limit($anime_review->review_title, 40)}}
-                            </h2>
-                            <div class="review-card-description-test" > 
-                              {!! Str::limit($anime_review->review_text, 160) !!}
+                                                <h2 class="review-card-title-test">
+                                                {{Str::limit($anime_review->review_title, 40)}}
+                                                </h2>
+                                                <div class="review-card-description-test" > 
+                                                {!! Str::limit($anime_review->review_text, 160) !!}
+                                                </div>
+                                                <br>
+                                                <br>
+
+                                            <span class="review-card-username-test"> 
+                                                    <img src='{{ URL::asset("images/user_images/{$anime_review->user_avatar}") }}' 
+                                                    class="rounded-circle" style="width: 57px; height:57px;"
+                                                    alt="Avatar" />
+                                                    {{$anime_review->user_name}}
+                                            </span> 
+                                            <br> 
+                                            <small class="review-card-anime-title-bottom-test">
+                                                    
+                                                @if($anime_review->english_title == null)
+                    
+                                                    {{$anime_review->japanese_title}}
+
+                                                    @else
+
+                                                    {{$anime_review->english_title}}
+
+                                                @endif
+
+                                            </small>
+
                             </div>
-                            <br>
-                            <br>
-
-                            <span class="review-card-username-test"> 
-                                    <img src='{{ URL::asset("images/user_images/{$anime_review->user_avatar}") }}' 
-                                    class="rounded-circle" style="width: 57px; height:57px;"
-                                    alt="Avatar" />
-                                    {{$anime_review->user_name}}
-                               </span> <br> <small class="review-card-anime-title-bottom-test">
-                                    
-                                @if($anime_review->english_title == null)
-    
-                                    {{$anime_review->japanese_title}}
-
-                                    @else
-
-                                    {{$anime_review->english_title}}
-
-                                @endif
-
-                                </small>
-
-            </div>
-    </div>
-
-  
-
-        @endforeach
-        
-        @else
-        
-         <div class="col-md-4">
-             <p   >No reviews yet!</p>
-         </div>
+                      </div>
  
-     @endif
+                        @endforeach
+                        
+                        @else
+                        
+                        <div class="col-md-4">
+                            <p   >No reviews yet!</p>
+                        </div>
+                
+                    @endif
         
     </div>
 </div>
-</div>
+ 
